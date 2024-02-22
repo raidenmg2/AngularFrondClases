052760a5dfb7c401239de8dc9bba22613a40f9b3
Luis Carlos Villalobos Romero <raidenmg2@gmail.com>
2024-02-21 19:33:21 -0500

    creando rama desarrollo

---
+++ src/app/components/filtros/filtros.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-filtros',
+  standalone: true,
+  imports: [],
+  templateUrl: './filtros.component.html',
+  styleUrl: './filtros.component.css'
+})
+export class FiltrosComponent {
+
+}
